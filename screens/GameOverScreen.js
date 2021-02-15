2b2d462541becddd29f3ff034a8eff9dc7a2b777
EduMoreno <emoreno@gc.axesor.com>
2021-02-15 20:26:56 +0100

    chapter4 working with network images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -10,8 +10,10 @@ const GameOverScreen = props => {
             <TitleText>The Game is Over!</TitleText>
             <View style={styles.imageContainer}>
             <Image 
+                fadeDuration={300}
                 style={styles.image} 
-                source={require('../assets/game-over.jpeg')}
+                //source={require('../assets/game-over.jpeg')}
+                source={{uri:'https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/2T0t-6V/videoblocks-game-over-8-bit-funky-a-funky-colorful-4k-game-over-screen-animation-letters-falling-towards-the-center-8-bit-retro-style-red-and-yellow_hcqx9_kax_thumbnail-full06.png'}}
                 resizeMode='cover'
             />
             </View>
